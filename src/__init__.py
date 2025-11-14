b818df73427a0e4d5a560d3c477c6095888660e1
Joel <jei.wils@googlemail.com>
2025-11-14 15:40:59 +0100

    1411 last laptop

--- src/__init__.py
+++ src/__init__.py
@@ -10,6 +10,7 @@ TO DO:
 
 
 - home
+= fix bugs in syntactics module - i think something with the new file structure and the graphing functionality 
 = how to integrate sliding window with token limit of llm? I could do the same window for each? 
 = coincide window analysis with LLM token budgets
 = make sure windows in the text analytics interface properly with the sliding window stuff - ie make sure the log probs, avg word frequency etc interface properly with the metrics from individual sentences in the lexico_semantics module 
